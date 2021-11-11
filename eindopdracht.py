9c78b6d21195a3321dc7e471a6bdf0d74355a2e1
scarlethau <91545401+scarlethau@users.noreply.github.com>
2021-11-11 17:56:54 +0100

    Update eindopdracht.py

--- eindopdracht.py
+++ eindopdracht.py
@@ -6,17 +6,17 @@
 
  # In[1]:
 import streamlit as st
- import folium
- import geopandas as gpd
- import pandas as pd
- from streamlit_folium import folium_static
- import plotly.express as px
- import plotly.graph_objects as go
- import numpy as np
- import requests
- import json
- import plotly.figure_factory as ff
- import statsmodels.api as sm
+import folium
+import geopandas as gpd
+import pandas as pd
+from streamlit_folium import folium_static
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import requests
+import json
+import plotly.figure_factory as ff
+import statsmodels.api as sm
 
 
  #https://www.kaggle.com/cityapiio/world-cities-average-internet-prices-2010-2020
